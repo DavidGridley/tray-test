f68e30e5f91b122391d805c0eec0c43f518b5035
David Gridley <davidgridley21@gmail.com>
2018-12-19 12:32:25 +0000

    seperated all input variables

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const util = require("util");
 
 const readInput = util.promisify(fs.readFile);
 
-function fetchInstructions() {
+function fetchInput() {
   readInput("input.txt", "utf8")
-    .then(input => console.log(input))
+    .then(input => inputToArray(input))
     .catch(error => console.log(error));
 }
 
-fetchInstructions();
+function inputToArray(input) {
+  const inputArray = input.split("\n");
+  seperateVariables(inputArray);
+}
+
+function seperateVariables(inputArray) {
+  const roomDimensions = inputArray[0].split(" ");
+  const roomX = roomDimensions[0];
+  const roomY = roomDimensions[1];
+  const hooverPosition = inputArray[1].split(" ");
+  const hooverX = hooverPosition[0];
+  const hooverY = hooverPosition[1];
+  const dirtPatches = inputArray.slice(2, (inputArray.length - 1))
+  .map(patch => patch.split(" "));
+  const hooverDirections = inputArray[inputArray.length - 1];
+}
+
+function calculateRoute() {
+    
+}
+
+fetchInput();
