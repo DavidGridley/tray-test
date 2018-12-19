ad4c999187cac46e3a8735a246fb18b2dc8ffc4e
David Gridley <davidgridley21@gmail.com>
2018-12-19 11:32:50 +0000

    reading input.txt with async readFile

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-const fs = require('file-system');
+const fs = require("fs");
+const util = require("util");
 
+const readInput = util.promisify(fs.readFile);
+
+function fetchInstructions() {
+  readInput("input.txt", "utf8")
+    .then(input => console.log(input))
+    .catch(error => console.log(error));
+}
+
+fetchInstructions();
